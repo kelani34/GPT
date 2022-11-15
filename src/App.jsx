f6176e0cb6ad4c1204367539bdaab4c4f76244b9
kelani34 <kelanitaiwo34@gmail.com>
2022-11-15 14:43:45 +0100

    Add: containers  and components

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,15 @@ function App() {
       <div className="App">
         <div className="gradient__bg">
           <NavBar />
+          <Header />
         </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
       </div>
     </>
   );
