87bfb752652b5270a8b421f5a3bc0da3468ef5d0
kelani34 <kelanitaiwo34@gmail.com>
2022-11-16 12:08:08 +0100

    add: template

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./header.css";
 
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+
 function Header() {
   return (
     <>
@@ -9,11 +12,23 @@ function Header() {
           <h1 className="gradient__text">
             Let's build something amazing with GPT-3 OpenAI
           </h1>
-          <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+          <p>
+            Yet bed any for travelling assistance indulgence unpleasing. Not
+            thoughts all exercise blessing. Indulgence way everything joy
+            alteration boisterous the attachment. Party we years to order allow
+            asked of.
+          </p>
           <div className="gpt3__header-content-input">
             <input type="email" placeholder="Your Email Address" />
             <button type="button">Get started</button>
           </div>
+          <div className="gpt3__header-content__people">
+            <img src={people} alt="people" />
+            <p> 1600+ people requested access a visit in the last 24 hours</p>
+          </div>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} />
         </div>
       </div>
     </>
