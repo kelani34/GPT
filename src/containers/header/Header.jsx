e221dce2a07b5740c760607df7fee41c2eaf7adf
kelani34 <kelanitaiwo34@gmail.com>
2022-11-16 11:15:53 +0100

    Add: header content

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <div id="home" className="gpt3__header section__padding">
+        <div className="gpt3__header-content">
+          <h1 className="gradient__text">
+            Let's build something amazing with GPT-3 OpenAI
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
