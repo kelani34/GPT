31e8aaa8026ec62d86e961a7bb0709cc4fb9bc55
kelani34 <kelanitaiwo34@gmail.com>
2022-11-16 20:02:54 +0100

    feat: Feature section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './whatgpt3.css'
-
+import React from "react";
+import "./whatgpt3.css";
+import { Feature } from "../../components";
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
-  )
+    <>
+      <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+        <div className="gpt3__whatgot3-feature">
+          <Feature />
+        </div>
+        <div className="gpt3__whatgpt3-heading">
+          <h1 className="gradient__text">
+            The possibilities beyond your imagination
+          </h1>
+          <p>Explore the library</p>
+          <div className="gpt3__whatgpt3-container">
+            <Feature />
+            <Feature />
+            <Feature />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default WhatGPT3
+export default WhatGPT3;
