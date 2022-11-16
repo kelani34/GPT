40cf935c5a6f56a5e50289e1abc8d17e1e2acf50
kelani34 <kelanitaiwo34@gmail.com>
2022-11-16 11:45:57 +0100

    add: header content

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -9,8 +9,10 @@ function Header() {
           <h1 className="gradient__text">
             Let's build something amazing with GPT-3 OpenAI
           </h1>
+          <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
           <div className="gpt3__header-content-input">
             <input type="email" placeholder="Your Email Address" />
+            <button type="button">Get started</button>
           </div>
         </div>
       </div>
