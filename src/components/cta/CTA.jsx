926e582205ade6f6c39db483cc63ac35a27fe6ed
kelani34 <kelanitaiwo34@gmail.com>
2022-12-15 22:09:22 +0100

    fix: cta styling

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,7 @@ import "./cta.css";
 
 function CTA() {
   return (
-    <div className="gpt3__cta section__padding">
+    <div className="gpt3__cta ">
       <div className="gpt3__cta-content">
         <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possiblities.</h3>
