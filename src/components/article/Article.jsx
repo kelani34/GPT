2442f899ef25c3748a68f318b9a46ac3aa68d5d9
kelani34 <kelanitaiwo34@gmail.com>
2022-12-20 23:55:29 +0100

    feat: add blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import './article.css'
+import "./article.css";
 
-function Article() {
-  return <div>Article</div>;
+function Article({ imgSRC }) {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgSRC} alt="blog image" />
+      </div>
+    </div>
+  );
 }
 
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './blog.css'
+import React from "react";
+import { Article } from "../../components";
+import "./blog.css";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 
 function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-contaiber_groupA">
+          <Article imgSRC={blog01}/>
+        </div>
+        <div className="gpt3__blog-contaiber_groupB">
+          <Article imgSRC={blog01}/>
+          <Article imgSRC={blog01}/>
+          <Article imgSRC={blog01}/>
+          <Article imgSRC={blog01}/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
