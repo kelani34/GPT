18128955c78e51a46ea26af2a2a72616ecede834
kelani34 <kelanitaiwo34@gmail.com>
2022-12-21 00:23:55 +0100

    feat: style blogs section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -12,14 +12,14 @@ function Blog() {
         </h1>
       </div>
       <div className="gpt3__blog-container">
-        <div className="gpt3__blog-contaiber_groupA">
+        <div className="gpt3__blog-container_groupA">
           <Article
             imgSRC={blog01}
             date="Oct 26, 2022"
             title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
           />
         </div>
-        <div className="gpt3__blog-contaiber_groupB">
+        <div className="gpt3__blog-container_groupB">
           <Article
             imgSRC={blog02}
             date="Oct 26, 2022"
